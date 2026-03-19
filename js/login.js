90776cda15fd47978655410a8f50be4e15aa1873
Jerónimo Falcón <jfalcondavila42@gmail.com>
2026-03-19 12:26:04 +0000

    register.js and login.js finally add content to register.html and login.html!!

--- js/login.js
+++ js/login.js
@@ -1,4 +1,26 @@
 document.addEventListener('DOMContentLoaded', async () => {
     await loadCommonTemplates();
     await loadTemplate('login-template', 'login');
+
+    const data = await fetchData('login');
+    if (!data) return false;
+
+    const container = document.getElementById('login');
+
+    container.querySelector('h1').textContent = data.login.title;
+
+    data.login.fields.forEach(field => {
+        const input = container.querySelector(`input[name="${field.name}"]`);
+        if (!input) return false;
+        input.closest('label').firstChild.textContent = field.label + ' ';
+        if (field.placeholder) input.placeholder = field.placeholder;
+    });
+
+    const spans = container.querySelectorAll('.register_password_forgot');
+
+    container.querySelector('.button').textContent = data.login.submitLabel;
+    spans[0].firstChild.textContent = data.login.forgotLink.text + '';
+    spans[0].querySelector('a').textContent = data.login.forgotLink.linkText;
+    spans[1].firstChild.textContent = data.login.registerLink.text + '';
+    spans[1].querySelector('a').textContent = data.login.registerLink.linkText;
 });
--- js/register.js
+++ js/register.js
@@ -1,4 +1,22 @@
 document.addEventListener('DOMContentLoaded', async () => {
     await loadCommonTemplates();
     await loadTemplate('register-template', 'register');
+
+    const data = await fetchData('register');
+    if (!data) return false;
+
+    const container = document.getElementById('register');
+
+    container.querySelector('h1').textContent = data.register.title;
+
+    data.register.fields.forEach(field => {
+        const input = container.querySelector(`input[name="${field.name}"]`);
+        if (!input) return false;
+        input.closest('label').firstChild.textContent = field.label + ' ';
+        if (field.placeholder) input.placeholder = field.placeholder;
+    });
+
+    container.querySelector('.button').textContent = data.register.submitLabel;
+    container.querySelector('a[href="login.html"]').textContent = data.register.loginLink.linkText;
+    container.querySelector('.register_password_forgot').firstChild.textContent = data.register.loginLink.text;
 });
