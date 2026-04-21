8d12dadbec1d2920d30a21a3a527efcd6ef2d6d4
Jerónimo Falcón <jfalcondavila42@gmail.com>
2026-04-21 18:00:20 +0100

    Angular version downgraded to 20

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import 'zone.js';
+import {Component, signal} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {HeaderComponent} from './components/header/header.component';
 import {FooterComponent} from './components/footer/footer.component';
 
