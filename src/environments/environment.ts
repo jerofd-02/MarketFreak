55f368f63062a23f167598b7dff97161a266f29e
Néstor <nedeglez75@gmail.com>
2026-04-22 10:21:38 +0100

    Defined database environment and first deployment of the webpage (not finished)

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyDWMq-zk2H1c-GOIUdmE6P3lOVbBQUqWDw",
+    authDomain: "marketfreak-58bf9.firebaseapp.com",
+    projectId: "marketfreak-58bf9",
+    storageBucket: "marketfreak-58bf9.firebasestorage.app",
+    messagingSenderId: "925002387132",
+    appId: "1:925002387132:web:920ab8d23d45f54a684ab9"
+  }
+};
