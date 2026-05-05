669ce75a9578d2e085ff9998347cd7977f064921
Jerónimo Falcón <jfalcondavila42@gmail.com>
2026-05-05 22:48:00 +0100

    Initial empty project of Ionic with the old files of sprint3 and no routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,31 +1,13 @@
-import {Routes} from '@angular/router';
-import {HomeComponent} from './pages/home/home.component';
-import {LoginComponent} from './components/login/login.component';
-import {RegisterComponent} from './components/register/register.component';
-import {ProductPage} from './pages/product-page/product-page.component';
-import {FaqComponent} from './components/faq/faq.component';
-import {WishlistComponent} from './pages/wishlist/wishlist.component';
-import {PaymentComponent} from './pages/payment/payment.component';
-import {ContactComponent} from './pages/contact/contact.component';
-import {UploadProductComponent} from './pages/upload-product/upload-product.component';
-import {SearchProductComponent} from './pages/search-product/search-product.component';
-import {ProfilePage} from './pages/profile/profile.component';
-import {UpdateProfile} from './pages/update-profile/update-profile.component';
-import {Confirmation} from './pages/confirmation/confirmation.component';
-import {authGuard} from './guards/auth-guard';
+import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
-  {path: 'product-page/:id', component: ProductPage},
-  {path: 'faq', component: FaqComponent},
-  {path: 'wishlist', component: WishlistComponent, canActivate: [authGuard]},
-  {path: 'payment/:id', component: PaymentComponent, canActivate: [authGuard]},
-  {path: 'contact', component: ContactComponent},
-  {path: 'upload-product', component: UploadProductComponent, canActivate: [authGuard]},
-  {path: 'search-product', component: SearchProductComponent},
-  {path: 'profile', component: ProfilePage},
-  {path: 'update-profile', component: UpdateProfile, canActivate: [authGuard]},
-  {path: 'confirmation', component: Confirmation, canActivate: [authGuard]},
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
 ];
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,16 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
 export const environment = {
-  firebase: {
-    apiKey: "AIzaSyDWMq-zk2H1c-GOIUdmE6P3lOVbBQUqWDw",
-    authDomain: "marketfreak-58bf9.firebaseapp.com",
-    projectId: "marketfreak-58bf9",
-    storageBucket: "marketfreak-58bf9.firebasestorage.app",
-    messagingSenderId: "925002387132",
-    appId: "1:925002387132:web:920ab8d23d45f54a684ab9"
-  }
+  production: false
 };
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
